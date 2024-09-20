29576b80894ee28696e678319c46afabeb8544d3
Patrick Lewis <4015312+locus313@users.noreply.github.com>
2024-09-19 23:39:20 -0700

    add git to windows install script

--- install.ps1
+++ install.ps1
@@ -3,5 +3,10 @@ if (!(Get-Command chezmoi -errorAction SilentlyContinue))
   winget install --source winget --accept-package-agreements twpayne.chezmoi
 }
 
+if (!(Get-Command git -errorAction SilentlyContinue))
+{
+  winget install --source winget --accept-package-agreements Git.Git
+}
+
 refreshenv | Out-Null
 chezmoi init --apply locus313
