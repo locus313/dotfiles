62301e87c3f53da3feeead065fa692f2e11b41a4
Patrick Lewis <4015312+locus313@users.noreply.github.com>
2024-09-19 23:48:26 -0700

    add pwsh to windows install script

--- install.ps1
+++ install.ps1
@@ -8,5 +8,10 @@ if (!(Get-Command git -errorAction SilentlyContinue))
   winget install --source winget --accept-package-agreements Git.Git
 }
 
+if (!(Get-Command pwsh -errorAction SilentlyContinue))
+{
+  winget install --source winget --accept-package-agreements Microsoft.PowerShell
+}
+
 refreshenv | Out-Null
 chezmoi init --apply locus313
